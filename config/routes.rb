dc8dc8b0cbd93152007aee6ed8b3143d5c4ed8bd
EZ1995 <138735267+EZ1995@users.noreply.github.com>
2023-08-14 23:58:45 -0600

    Log in functional

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "sessions#new"
+  root "home#index"
 end
